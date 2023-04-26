6390e7991dcd0c69a85ec670c5d5cfa5e6cf4e4a
Miguel Yuste <yusaguilar@gmail.com>
2023-04-26 12:20:07 +0200

    259. Estructura de directorios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'selector', loadChildren: () => import('./paises/paises.module').then((m) => m.PaisesModule) },
+  { path: '**', redirectTo: 'selector' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
